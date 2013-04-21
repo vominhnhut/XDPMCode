bcde336c5f1b034216329b41d70bddc2658fd925
hoaphat <phatnguyen.it.khtn@gmail.com>
2013-04-22 00:37:04 +0700

    sprite bird, pig, block. bao gồm cả hiệu ứng heo nổ, chim đen nổ, lông chim.

---
+++ Resources/sprite/BLOCK/Block.java
@@ -0,0 +1,74 @@
+// Definitions for sprite sheet Block
+// Created with www.texturepacker.com
+
+// $TexturePacker:SmartUpdate:cdce16093ec9c7978734fe2a3f2b04da$
+
+package Texture;
+
+public interface Block
+{
+	public static final int BALLBLACK_ID = 0;
+	public static final int BALLRED_ID = 1;
+	public static final int BANANA_ID = 2;
+	public static final int BERRY_ID = 3;
+	public static final int CHEST_ID = 4;
+	public static final int CIRBIG_ICE_ID = 5;
+	public static final int CIRBIG_ROCK_ID = 6;
+	public static final int CIRBIG_WOOD_ID = 7;
+	public static final int CIRMEDIUM_ICE_ID = 8;
+	public static final int CIRMEDIUM_ROCK_ID = 9;
+	public static final int CIRMEDIUM_WOOD_ID = 10;
+	public static final int CUP_ID = 11;
+	public static final int DIAMOND_ID = 12;
+	public static final int EGG_ID = 13;
+	public static final int EGGGOLDEN_ID = 14;
+	public static final int FLAG_ID = 15;
+	public static final int FLAG_VN_ID = 16;
+	public static final int GROUNDBIG_ID = 17;
+	public static final int GROUNDLONG_ID = 18;
+	public static final int GROUNDMEDIUM_ID = 19;
+	public static final int GROUNDSHORT_ID = 20;
+	public static final int GROUNDSMALL_ID = 21;
+	public static final int ICE_FRAGMENT_ID = 22;
+	public static final int LEMON_ID = 23;
+	public static final int RECEMPTY_ICE_ID = 24;
+	public static final int RECEMPTY_ROCK_ID = 25;
+	public static final int RECEMPTY_WOOD_ID = 26;
+	public static final int RECFULL_ICE_ID = 27;
+	public static final int RECFULL_ROCK_ID = 28;
+	public static final int RECFULL_WOOD_ID = 29;
+	public static final int RECLONG_ICE_ID = 30;
+	public static final int RECLONG_ROCK_ID = 31;
+	public static final int RECLONG_WOOD_ID = 32;
+	public static final int RECMEDIUM_ICE_ID = 33;
+	public static final int RECMEDIUM_ROCK_ID = 34;
+	public static final int RECMEDIUM_WOOD_ID = 35;
+	public static final int RECSHORT_ICE_ID = 36;
+	public static final int RECSHORT_ROCK_ID = 37;
+	public static final int RECSHORT_WOOD_ID = 38;
+	public static final int RECSHORTEST_ICE_ID = 39;
+	public static final int RECSHORTEST_ROCK_ID = 40;
+	public static final int RECSHORTEST_WOOD_ID = 41;
+	public static final int ROCK_FRAGMENT_ID = 42;
+	public static final int SQUAREMEDIUM_ICE_ID = 43;
+	public static final int SQUAREMEDIUM_ROCK_ID = 44;
+	public static final int SQUAREMEDIUM_WOOD_ID = 45;
+	public static final int SQUARESMALL_ICE_ID = 46;
+	public static final int SQUARESMALL_ROCK_ID = 47;
+	public static final int SQUARESMALL_WOOD_ID = 48;
+	public static final int TNT_ID = 49;
+	public static final int TRIAEMPTY_ICE_ID = 50;
+	public static final int TRIAEMPTY_ROCK_ID = 51;
+	public static final int TRIAEMPTY_WOOD_ID = 52;
+	public static final int TRIAFULL_ICE_ID = 53;
+	public static final int TRIAFULL_ROCK_ID = 54;
+	public static final int TRIAFULL_WOOD_ID = 55;
+	public static final int WATERMELON_ID = 56;
+	public static final int WHEEL_ID = 57;
+	public static final int WHEELMEDIUM_ID = 58;
+	public static final int WHEELSMALL_ID = 59;
+	public static final int WOOD_FRAGMENT_ID = 60;
+	public static final int OTHER_1_ID = 61;
+	public static final int OTHER_2_ID = 62;
+	public static final int OTHER_3_ID = 63;
+}
