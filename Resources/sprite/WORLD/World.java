ec32020a0af194e9398252b9ec5e18a669622173
hoaphat <phatnguyen.it.khtn@gmail.com>
2013-04-29 02:38:03 +0700

    Resource mới cho giao diện gồm Button, số, world,...

---
+++ Resources/sprite/WORLD/World.java
@@ -0,0 +1,21 @@
+// Definitions for sprite sheet World
+// Created with www.texturepacker.com
+
+// $TexturePacker:SmartUpdate:57780551f0402f461caf620f8ba8a112$
+
+package Texture;
+
+public interface World
+{
+	public static final int LOCKWORLD_ID = 0;
+	public static final int SCOREWORLD_ID = 1;
+	public static final int STARWORLD_ID = 2;
+	public static final int WORLD1_ID = 3;
+	public static final int WORLD2_ID = 4;
+	public static final int WORLD3_ID = 5;
+	public static final int WORLD4_ID = 6;
+	public static final int WORLD5_ID = 7;
+	public static final int WORLD6_ID = 8;
+	public static final int WORLD7_ID = 9;
+	public static final int WORLD8_ID = 10;
+}
