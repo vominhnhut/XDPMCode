e458a3614ae4e762325f834e1d13da6bbb20dbc4
hoaphat <phatnguyen.it.khtn@gmail.com>
2013-04-29 18:45:44 +0700

    bổ sung resource

---
+++ Resources/sprite/SCORE/Score.java
@@ -0,0 +1,23 @@
+// Definitions for sprite sheet Score
+// Created with www.texturepacker.com
+
+// $TexturePacker:SmartUpdate:82a77bd0e378d6584443c83055308d86$
+
+package Texture;
+
+public interface Score
+{
+	public static final int SCORE_10K_BLACK_ID = 0;
+	public static final int SCORE_10K_BLUE_ID = 1;
+	public static final int SCORE_10K_GREEN_ID = 2;
+	public static final int SCORE_10K_ORANGE_ID = 3;
+	public static final int SCORE_10K_PINK_ID = 4;
+	public static final int SCORE_10K_RED_ID = 5;
+	public static final int SCORE_10K_WHITE_ID = 6;
+	public static final int SCORE_10K_YELLOW_ID = 7;
+	public static final int SCORE_3K_PINK_ID = 8;
+	public static final int SCORE_3K_PURPLE_ID = 9;
+	public static final int SCORE_3K_RED_ID = 10;
+	public static final int SCORE_3K_YELLOW_ID = 11;
+	public static final int SCORE_5K_GREEN_ID = 12;
+}
