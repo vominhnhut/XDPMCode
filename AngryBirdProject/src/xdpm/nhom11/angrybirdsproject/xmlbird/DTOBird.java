0a9e3aa1571b8af26b11b03a4aca583d3210fddc
ngcongphuc <ngcongphuc@gmail.com>
2013-05-01 21:54:07 +0700

    Load dữ liệu game, điểm số từ xml Chưa thay đổi được điểm số từng level (mệt)

---
+++ AngryBirdProject/src/xdpm/nhom11/angrybirdsproject/xmlbird/DTOBird.java
@@ -0,0 +1,6 @@
+package xdpm.nhom11.angrybirdsproject.xmlbird;
+
+public class DTOBird {
+	public int id;
+	public double finalscore;
+}
---
+++ AngryBirdProject/src/xdpm/nhom11/angrybirdsproject/xmlbird/DTOBlock.java
@@ -0,0 +1,7 @@
+package xdpm.nhom11.angrybirdsproject.xmlbird;
+
+public class DTOBlock {
+	public int id;
+	public int lifevalue;
+	public double score;
+}
